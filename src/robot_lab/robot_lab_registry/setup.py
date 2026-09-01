df5f3a613f2ee4d7e84065167d08c24e8d7c8c5c
Arman10121995 <arman10121995@gmail.com>
2026-09-01 19:39:57 +0200

    Fix full-workspace colcon build symlink-install failures

--- src/robot_lab/robot_lab_registry/setup.py
+++ src/robot_lab/robot_lab_registry/setup.py
@@ -10,7 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/config', ['config/robots.yaml', 'config/environments.yaml', 'config/scenarios.yaml', 'config/experiments.yaml']),
+        ('share/' + package_name + '/config', ['config/robots.yaml', 'config/environments.yaml', 'config/scenarios.yaml', 'config/experiments.yaml', 'config/algorithms.yaml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
